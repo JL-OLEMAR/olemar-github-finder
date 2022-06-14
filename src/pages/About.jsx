df969896c85f4d40a0f70822626eec9e8a616a86
JL-OLEMAR <joseluis19963@gmail.com>
2022-06-13 20:48:53 -0500

    feat: Add About component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,24 @@
 export function About () {
   return (
-    <div>About</div>
+    <>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details.
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version: <span className='text-gray-400'>1.0.0</span>
+      </p>
+      <p className='text-lg text-gray-400'>
+        Developed by: {' '}
+        <a
+          className='text-gray-400'
+          href='https://github.com/JL-OLEMAR/'
+          rel='noreferrer'
+          target='_blank'
+        >
+          JL-OLEMAR
+        </a>
+      </p>
+    </>
   )
 }
